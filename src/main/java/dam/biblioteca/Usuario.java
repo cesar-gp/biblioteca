d6af45f99cef1089254c9e877df28985c0f322a5
cesar-gp <cesar.gutierrez.merida@gmail.com>
2026-02-10 20:34:33 +0100

    Añadido: registrar y eliminar usuarios

--- src/main/java/dam/biblioteca/Usuario.java
+++ src/main/java/dam/biblioteca/Usuario.java
@@ -4,7 +4,7 @@ public class Usuario {
 
 	// Propiedades estáticas
 
-	private static Usuario[] lista;
+	private static Usuario[] lista = new Usuario[0];
 
 	// Propiedades no estáticas
 
@@ -15,12 +15,17 @@ public class Usuario {
 	// Constructor
 
 	public Usuario(String nombre, String contrasena, boolean admin) {
+		// Asignar propiedades
 		this.nombre = nombre;
 		this.contrasena = contrasena;
 		this.admin = admin;
 	}
 
 	// Getters
+
+	public static Usuario[] getUsuarios() {
+		return lista;
+	}
 
 	public String getNombre() {
 		return this.nombre;
@@ -36,6 +41,54 @@ public class Usuario {
 		if(!this.contrasena.equals(contrasena)) return false;
 
 		this.nombre = newNombre;
+		return true;
+	}
+
+	// Funciones
+
+	public boolean registrar() {
+		// Evitar overflow.
+		if(lista.length == Integer.MAX_VALUE) return false;
+
+		// Crear copia de la lista de usuarios con un hueco extra.
+		Usuario[] copia = new Usuario[lista.length + 1];
+
+		// Copiar los valores en ella.
+		for(int i = 0; i < lista.length; i++)
+			copia[i] = lista[i];
+
+		// Meter este usuario al final.
+		copia[lista.length] = this;
+
+		// Sustituir la lista original por la copia.
+		lista = copia;
+
+		// Operación realizada.
+		return true;
+	}
+
+	public boolean eliminar() {
+		// Buscar índice del usuario en la lista.
+		int indice = -1;
+		for(int i = 0; i < lista.length && indice == -1; i++)
+			if(lista[i].nombre == this.nombre)
+				indice = i;
+
+		// ¿El usuario no está registrado? Error.
+		if(indice == -1) return false;
+
+		// Crear copia de la lista sin el elemento de ese indice.
+		Usuario[] copia = new Usuario[lista.length - 1];
+		for(int i = 0; i < indice; i++)
+			copia[i] = lista[i];
+
+		for(int i = indice; i < lista.length - 1; i++)
+			copia[i] = lista[i + 1];
+
+		// Sustituir lista original por la copia.
+		lista = copia;
+
+		// Operación realizada.
 		return true;
 	}
 }
